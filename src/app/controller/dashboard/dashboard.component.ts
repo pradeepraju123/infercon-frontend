e1049c1e4e1cf2f49d2f910f932b5809b392c838
pradeepraju123 <rpradeep1797@gmail.com>
2024-03-18 22:55:46 +0530

    changes in font style

--- src/app/controller/dashboard/dashboard.component.ts
+++ src/app/controller/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,12 +9,33 @@ import { UserService } from '../../services/user.service';
 export class DashboardComponent implements OnInit {
 
   users: any[]=[]; // Define a property to store the fetched data
-
+  chart: any = [];
   constructor(private userService: UserService) { };
 
   ngOnInit(): void {
     this.userService.getAllUsers().subscribe((data: any) => {
       this.users = data.data;
     });
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
   }
+  
 }
