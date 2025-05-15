7a14eeb29cdf783bbdf280dfa6a4f3ebf4f8d0ea
nanthini <nanthini@trstscore.com>
2025-05-15 22:47:45 +0530

    whatsapp

--- src/app/services/whatsapp-activity.service.ts
+++ src/app/services/whatsapp-activity.service.ts
@@ -10,84 +10,82 @@ export class WhatsappActivityService {
 
   
   private apiUrl = 'https://api.inferconautomation.com/api/v1/contact';
-  // private excelUrl='https://api.inferconautomation.com/api/v1/users/getall';
-  // private excelUrl1='https://api.inferconautomation.com/api/v1/users/bulkwhatsmes';
-  private excelUrl='http://localhost:8081/api/v1/users/getall';
-  private excelUrl1='http://localhost:8081/api/v1/users/bulkwhatsmes';
-  // private excelUrl_contacts='https://api.inferconautomation.com/api/v1/users/bulkupload';
-    private excelUrl_contacts='http://localhost:8081/api/v1/users/bulkupload';
+  private excelUrl='https://api.inferconautomation.com/api/v1/users/getall';
+  private excelUrl1='https://api.inferconautomation.com/api/v1/users/bulkwhatsmes';
+  //private excelUrl='http://localhost:8081/api/v1/users/getall';
+  //private excelUrl1='http://localhost:8081/api/v1/users/bulkwhatsmes';
+   private excelUrl_contacts='https://api.inferconautomation.com/api/v1/users/bulkupload';
+    //private excelUrl_contacts='http://localhost:8081/api/v1/users/bulkupload';
 
 
   constructor(
     private http: HttpClient
   ) {}
   
-  uploaduser(data: any): Observable<any> {
-    // const token = sessionStorage.getItem('authToken');
-    const url = `${this.excelUrl_contacts}`;
-    
-     
-      return this.http.post(url, data,);
-   
-   
-  }
-  
   // uploaduser(data: any): Observable<any> {
-  //   const token = sessionStorage.getItem('authToken');
+  //   // const token = sessionStorage.getItem('authToken');
   //   const url = `${this.excelUrl_contacts}`;
-  //   if(token)
-  //   {
-  //     const headers = new HttpHeaders({
-  //       'Authorization': 'Bearer ' + token // Include the token in the request headers
-  //         });
-  //     return this.http.post(url, data, { headers });
-  //   }
-  //   else{
-  //     return throwError('No authentication token found'); 
-  //   }
+  //     return this.http.post(url, data,);
    
-  //  }
-  // getAllContact(data: any): Observable<any> {
-  //   const token = sessionStorage.getItem('authToken');
-
-  //   if (token) {
-  //     const headers = new HttpHeaders({
-  //       'Authorization': 'Bearer ' + token
-  //         });
-  //   return this.http.post(`${this.excelUrl}/get`, data, { headers });
-  // } else {
- 
-  //    return throwError('No authentication token found'); 
-  //   }
+   
   // }
+  
+  uploaduser(data: any): Observable<any> {
+    const token = sessionStorage.getItem('authToken');
+    const url = `${this.excelUrl_contacts}`;
+    if(token)
+    {
+      const headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + token // Include the token in the request headers
+          });
+      return this.http.post(url, data, { headers });
+    }
+    else{
+      return throwError('No authentication token found'); 
+    }
+   
+   }
+  getAllContact(data: any): Observable<any> {
+    const token = sessionStorage.getItem('authToken');
 
-  // sendmessage_filtercontact(data: any): Observable<any> {
-  //   const token = sessionStorage.getItem('authToken');
-
-  //   if (token) {
-  //     const headers = new HttpHeaders({
-  //       'Authorization': 'Bearer ' + token
-  //         });
-  //   return this.http.post(`${this.excelUrl1}/get`, data, { headers });
-  // } else {
+    if (token) {
+      const headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + token
+          });
+    return this.http.post(`${this.excelUrl}/get`, data, { headers });
+  } else {
  
-  //    return throwError('No authentication token found'); 
-  //   }
-  // }
-  sendmessage_filtercontact(data: any): Observable<any> {
-    const url = `${this.excelUrl1}`;
-    return this.http.post(url,data);
-
+     return throwError('No authentication token found'); 
+    }
   }
 
-  getAllContact(): Observable<any> {
-    const url = `${this.excelUrl}`;
+  sendmessage_filtercontact(data: any): Observable<any> {
+    const token = sessionStorage.getItem('authToken');
+
+    if (token) {
+      const headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + token
+          });
+    return this.http.post(`${this.excelUrl1}/get`, data, { headers });
+  } else {
+ 
+     return throwError('No authentication token found'); 
+    }
+  }
+  // sendmessage_filtercontact(data: any): Observable<any> {
+  //   const url = `${this.excelUrl1}`;
+  //   return this.http.post(url,data);
+
+  // }
+
+  // getAllContact(): Observable<any> {
+  //   const url = `${this.excelUrl}`;
 
       
-    return this.http.get(url);
+  //   return this.http.get(url);
   
  
-  }
+  // }
 
   
 }
