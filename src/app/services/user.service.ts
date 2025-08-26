7a93f6774d4b22097f88d933910e08012fb3f86e
pradeepraju123 <rpradeep1797@gmail.com>
2025-08-26 14:24:56 +0530

    possible fix

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -131,7 +131,7 @@ getDashboardData(params?: {
       if (params.newEnrollmentPage) queryParams.newEnrollmentPage = params.newEnrollmentPage;
       if (params.newEnrollmentLimit) queryParams.newEnrollmentLimit = params.newEnrollmentLimit;
     }
-    return this.http.get<any>('http://localhost:8081/api/v1/dashboard', {
+    return this.http.get<any>('https://api.inferconautomation.com/api/v1/dashboard', {
       headers,
       params: queryParams
     });
