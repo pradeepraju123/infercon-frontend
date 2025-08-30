74854f6e155f3275935d2a102414ff92455583a1
soniaJ <jsonia.infercon@gmail.com>
2025-08-30 15:05:28 +0530

    update model box

--- src/app/components/account-dialog/account-dialog.component.ts
+++ src/app/components/account-dialog/account-dialog.component.ts
@@ -1,4 +1,4 @@
-// account-dialog.component.ts
+
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { AccountsService } from '../../services/accounts.service';
